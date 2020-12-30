7c5298dbc4a03042a73b33f97a2a4fec9a208b75
goro9 <sokenbiccha@icloud.com>
2020-12-30 13:14:12 +0900

    excercise 9

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,6 @@ const app: express.Express = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('hello.ejs');
-});
-
-app.get('/top', (req, res) => {
     res.render('top.ejs');
 });
 
